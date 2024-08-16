4b0c284edfd7152506f3bc3feaa0f332988d2cc8
Daniel Chan <daniel.hoyin.chan@gmail.com>
2024-08-16 14:22:38 -0700

    working commit

---
+++ app/Provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+import React from "react";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <LiveblocksProvider
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+      authEndpoint={"/api/liveblocks-auth"}
+    >
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
